631005b6d6d4aade1e8155e5774a082d3dbf3281
WQH <1737082708@qq.com>
2022-06-03 17:01:45 +0800

    新增主页面

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="height: 50px;line-height: 50px;border-bottom: 1px solid #ccc;display: flex">
+    <div style="flex: 200px;padding-left: 10px;font-weight: bold;color: aqua">FTB商城</div>
+
+    <div style="flex: 1"></div>
+
+    <div style="flex: 100px;">
+      <el-dropdown>
+      <el-button type="primary">
+        用户<i class="el-icon-arrow-down el-icon--right"></i>
+      </el-button>
+      <el-dropdown-menu slot="dropdown">
+        <el-dropdown-item>个人信息</el-dropdown-item>
+        <el-dropdown-item></el-dropdown-item>
+        <el-dropdown-item></el-dropdown-item>
+        <el-dropdown-item></el-dropdown-item>
+        <el-dropdown-item>退出</el-dropdown-item>
+      </el-dropdown-menu>
+    </el-dropdown>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,39 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <el-table
+        :data="tableData"
+        stripe
+        style="width: 100%">
+      <el-table-column
+          prop="date"
+          label="日期"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="name"
+          label="姓名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="address"
+          label="地址">
+      </el-table-column>
+    </el-table>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
+import Vue from 'vue';
+import Element from 'element-ui';
+import HelloWorld from '@/components/HelloWorld.vue'
 export default {
   name: 'HomeView',
   components: {
     HelloWorld
   }
 }
+
+Vue.use(Element, { size: 'small', zIndex: 3000 });
+
 </script>
