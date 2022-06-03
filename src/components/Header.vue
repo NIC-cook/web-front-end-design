67d3b94940156a8d08e8cec97eb84e355147a768
WQH <1737082708@qq.com>
2022-06-03 17:41:10 +0800

    优化主页页面

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,29 +1,43 @@
 <template>
-  <div style="height: 50px;line-height: 50px;border-bottom: 1px solid #ccc;display: flex">
-    <div style="flex: 200px;padding-left: 10px;font-weight: bold;color: aqua">FTB商城</div>
+     <el-menu
+        :default-active="activeIndex2"
+        class="el-menu-demo"
+        mode="horizontal"
+        @select="handleSelect"
+        background-color="#545c64"
+        text-color="#fff"
+        active-text-color="#ffd04b">
+      <el-menu-item index="1">主页</el-menu-item>
+      <el-submenu index="2">
+        <template slot="title">分类</template>
+        <el-menu-item index="2-1">选项1</el-menu-item>
+        <el-menu-item index="2-2">选项2</el-menu-item>
+        <el-menu-item index="2-3">选项3</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="3" >消息中心</el-menu-item>
+      <el-menu-item index="4">订单管理</el-menu-item>
+       <el-submenu index="5">
+         <template slot="title">用户</template>
+         <el-menu-item index="2-1">账号信息</el-menu-item>
+         <el-menu-item index="2-2">登出</el-menu-item>
+       </el-submenu>
+     </el-menu>
 
-    <div style="flex: 1"></div>
-
-    <div style="flex: 100px;">
-      <el-dropdown>
-      <el-button type="primary">
-        用户<i class="el-icon-arrow-down el-icon--right"></i>
-      </el-button>
-      <el-dropdown-menu slot="dropdown">
-        <el-dropdown-item>个人信息</el-dropdown-item>
-        <el-dropdown-item></el-dropdown-item>
-        <el-dropdown-item></el-dropdown-item>
-        <el-dropdown-item></el-dropdown-item>
-        <el-dropdown-item>退出</el-dropdown-item>
-      </el-dropdown-menu>
-    </el-dropdown>
-    </div>
-  </div>
 </template>
 
 <script>
 export default {
-  name: "Header"
+  data() {
+    return {
+      activeIndex: '1',
+      activeIndex2: '1'
+    };
+  },
+  methods: {
+    handleSelect(key, keyPath) {
+      console.log(key, keyPath);
+    }
+  }
 }
 </script>
 
