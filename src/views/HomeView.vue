abdb0653ac5c1d9ad2d8770c08fd81aa107429a9
WQH <1737082708@qq.com>
2022-06-04 18:00:04 +0800

    优化主界面表格

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -14,52 +14,42 @@
         </div>
 
         <!--表格部分 -->
-        <el-table
-            :data="tableData"
-            style="width: 100%"
-            border>
-          <el-table-column type="expand">
-            <template slot-scope="props">
-              <el-form label-position="left" inline class="demo-table-expand">
-                <el-form-item label="商品名称">
-                  <span>{{ props.row.name }}</span>
-                </el-form-item>
-                <el-form-item label="所属店铺">
-                  <span>{{ props.row.shop }}</span>
-                </el-form-item>
-                <el-form-item label="商品 ID">
-                  <span>{{ props.row.id }}</span>
-                </el-form-item>
-                <el-form-item label="店铺 ID">
-                  <span>{{ props.row.shopId }}</span>
-                </el-form-item>
-                <el-form-item label="商品分类">
-                  <span>{{ props.row.category }}</span>
-                </el-form-item>
-                <el-form-item label="店铺地址">
-                  <span>{{ props.row.address }}</span>
-                </el-form-item>
-                <el-form-item label="商品描述">
-                  <span>{{ props.row.desc }}</span>
-                </el-form-item>
-              </el-form>
-            </template>
-          </el-table-column>
-          <el-table-column
-              label="商品 ID"
-              prop="id"
-              sortable
-          >
-          </el-table-column>
-          <el-table-column
-              label="商品名称"
-              prop="name">
-          </el-table-column>
-          <el-table-column
-              label="描述"
-              prop="desc">
-          </el-table-column>
-        </el-table>
+    <el-table
+        :data="tableData"
+        stripe
+        border
+        style="width: 100%">
+      <el-table-column
+          prop="accountId"
+          label="用户ID"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="accountName"
+          label="用户名"
+          width="180">
+      </el-table-column>
+      <el-table-column
+          prop="password"
+          label="密码">
+      </el-table-column>
+      <el-table-column
+          prop="power"
+          label="权限">
+      </el-table-column>
+      <el-table-column
+          prop="phone"
+          label="手机号">
+      </el-table-column>
+      <el-table-column
+          prop="email"
+          label="邮箱">
+      </el-table-column>
+      <el-table-column
+          prop="orderListId"
+          label="订单表ID">
+      </el-table-column>
+    </el-table>
 
         <el-pagination
             @size-change="handleSizeChange"
